9431efa9324b143ffc8a00daab78d52eb26c45f4
bobbyhiddn <mlmicahlongmire@gmail.com>
2023-07-21 13:33:38 -0600

    Revising setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='magi_cli',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     package_data={'magi_cli': ['artifacts/*.png']},
     include_package_data=True,
