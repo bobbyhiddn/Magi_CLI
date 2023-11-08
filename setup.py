7c8787836e26656eb1dfb031ebfed1079e7ba61f
bobbyhiddn <119532997+bobbyhiddn@users.noreply.github.com>
2023-11-07 19:59:17 -0700

    Update setup.py
    Trying to fix actions.

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
+# Read the contents of your README file
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='magi_cli',
-    version='0.1.1',
+    version='0.2',  # Make sure to increment this if you are republishing
     packages=find_packages(),
     package_data={'magi_cli': ['artifacts/*.png']},
     include_package_data=True,
@@ -16,13 +20,16 @@ setup(
         'flask',
         'PyQt5',
     ],
-    entry_points='''
-        [console_scripts]
-        cast=magi_cli.cast:cast
-    ''',
+    entry_points={
+        'console_scripts': [
+            'cast=magi_cli.cast:cast',
+        ],
+    },
     author="Micah Longmire",
     author_email="mlmicahlongmire@gmail.com",
     description="Magi_CLI is a command line interface for those who desire software development to feel more like magic.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Ensure this is correct
     url="https://github.com/bobbyhiddn/Magi_CLI",
     classifiers=[
         "Programming Language :: Python :: 3",
